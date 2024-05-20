80fc95d4b5dd45ea4af37e0bcf241a41b73d2003
re5nardo <re5nardo@naver.com>
2024-05-20 22:10:46 +0900

    EnvironmentSettings 구현

---
+++ Assets/Scripts/EnvironmentSettings.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    [CreateAssetMenu(fileName = "EnvironmentSettings", menuName = "LOP/Internal/Environment Settings")]
+    public class EnvironmentSettings : ScriptableObject
+    {
+        public static EnvironmentSettings _active;
+        public static EnvironmentSettings active
+        {
+            get
+            {
+                if (_active == null)
+                {
+                    _active = Resources.Load<EnvironmentSettings>($"EnvironmentSettings/EnvironmentSettings.Dev");
+                }
+                return _active;
+            }
+        }
+
+        [Serializable]
+        public class BaseURLSetting
+        {
+            public string scheme;
+            public string host;
+
+            public string baseUrl => $"{scheme}://{host}";
+        }
+
+        [SerializeField] private BaseURLSetting lobbyServerSetting;
+        [SerializeField] private BaseURLSetting matchmakingServerSetting;
+        [SerializeField] private BaseURLSetting roomServerSetting;
+
+        public string lobbyBaseURL => lobbyServerSetting.baseUrl;
+        public string matchmakingBaseURL => matchmakingServerSetting.baseUrl;
+        public string roomBaseURL => roomServerSetting.baseUrl;
+    }
+}

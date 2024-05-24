a3fce2ee05c595135876a70a43b1fc6bb3119f3e
re5nardo <re5nardo@naver.com>
2024-05-24 20:22:34 +0900

    Data 추가

---
+++ Assets/Scripts/Data/Data.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    public partial class Data
+    {
+        public static UserData User = new UserData();
+    }
+}
---
+++ Assets/Scripts/Data/UserData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    public partial class UserData
+    {
+        public User user;
+    }
+}

167a737e17629b1f73e58340a1ce087d6590bc48
re5nardo <re5nardo@naver.com>
2024-06-09 20:17:53 +0900

    loadingCanvas 추가

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -8,13 +8,16 @@ namespace LOP
     [DontDestroyMonoSingleton]
     public class CanvasManager : MonoSingleton<CanvasManager>
     {
-        [SerializeField] private Canvas popupCanvas;
-        [SerializeField] private Canvas toastCanvas;
-        [SerializeField] private Canvas systemCanvas;
+        [SerializeField] private Canvas _popupCanvas;
+        [SerializeField] private Canvas _loadingCanvas;
+        [SerializeField] private Canvas _toastCanvas;
+        [SerializeField] private Canvas _systemCanvas;
+
+        public Canvas loadingCanvas => _loadingCanvas;
 
         private void Start()
         {
-            PopupManager.instance.popupCanvas = popupCanvas;
+            PopupManager.instance.popupCanvas = _popupCanvas;
         }
     }
 }

c2cf61696ae1d6c747a02935c31465fff2101e36
re5nardo <re5nardo@naver.com>
2024-10-06 14:25:45 +0900

    서버에 맞춰 Dto 수정

--- Assets/Scripts/WebAPI/Model/MatchDto.cs
+++ Assets/Scripts/WebAPI/Model/MatchDto.cs
@@ -12,7 +12,7 @@ namespace LOP
         public MatchType matchType;
         public string subGameId;
         public string mapId;
-        public MatchStatus status;
+        public int targetRating;
         public string[] playerList;
     }
 
@@ -21,13 +21,5 @@ namespace LOP
     {
         Friendly = 0,
         Rank = 1,
-    }
-
-    [Serializable]
-    public enum MatchStatus
-    {
-        None = 0,
-        MatchStart = 1,
-        MatchEnd = 2,
     }
 }

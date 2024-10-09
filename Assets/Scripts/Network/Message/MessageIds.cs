bb25280a9bf10b37fa187bf4256c11bc3300e0fd
re5nardo <re5nardo@naver.com>
2024-10-09 11:23:03 +0900

    MessageIds 추가

---
+++ Assets/Scripts/Network/Message/MessageIds.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    public static class MessageIds
+    {
+        public const ushort GameInfoRequest = 1;
+        public const ushort GameInfoResponse = 2;
+    }
+}

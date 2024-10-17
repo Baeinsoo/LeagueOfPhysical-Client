2754426da1afd10b43dee42926aabc6451640f2b
re5nardo <re5nardo@naver.com>
2024-10-17 23:14:32 +0900

    LOPRoom 기본 구현

--- Assets/Scripts/Room/LOPRoom.cs
+++ Assets/Scripts/Room/LOPRoom.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
 using GameFramework;
+using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -20,43 +23,56 @@ namespace LOP
 
         private async void Awake()
         {
-            await InitializeAsync();
-
-            StartGame();
+            try
+            {
+                await InitializeAsync();
+                await ConnectRoomServerAsync();
+                await JoinRoomServerAsync();
+                await StartGameAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                SceneManager.LoadScene("Lobby");
+            }
         }
 
         private async void OnDestroy()
         {
-            StopGame();
-
+            await DisconnectRoomServerAsync();
             await DeinitializeAsync();
         }
 
         public async Task InitializeAsync()
         {
+            await game.InitializeAsync();
+
+#if UNITY_EDITOR
+            if (Blackboard.Contain<RoomDto>() == false)
+            {
+                var roomMeta = new RoomDto
+                {
+                    id = "test",
+                    matchId = "",
+                    ip = "localhost",
+                    port = 7777,
+                };
+                Data.Room.room = roomMeta;
+            }
+#endif
             Data.Room.room = Blackboard.Read<RoomDto>(erase: true);
 
             var getMatch = await WebAPI.GetMatch(Data.Room.room.matchId);
             if (getMatch.response.code != ResponseCode.SUCCESS)
             {
-                throw new System.Exception($"GetMatch Error. code: {getMatch.response.code}");
+                throw new Exception($"GetMatch Error. code: {getMatch.response.code}");
             }
+
             Data.Room.match = getMatch.response.match;
-
-            networkManager.networkAddress = Data.Room.room.ip;
-            networkManager.port = Data.Room.room.port;
-
-            networkManager.onStartClient += () =>
-            {
-            };
-            networkManager.onStopClient += () =>
-            {
-                SceneManager.LoadScene("Lobby");
-            };
 
             roomNetwork.RegisterHandler<GameInfoResponse>(OnGameInfoResponse);
 
-            await game.InitializeAsync();
+            game.onGameEnd += OnGameEnd;
 
             initialized = true;
         }
@@ -65,33 +81,73 @@ namespace LOP
         {
             await game.DeinitializeAsync();
 
+            Data.Room.Clear();
+
             try
             {
                 roomNetwork.UnregisterHandler<GameInfoResponse>(OnGameInfoResponse);
             }
             catch
             {
-                //  Maybe roomNetwork was destroyed already
+                //  Maybe roomNetwork was already destroyed.
             }
 
-            Data.Room.Clear();
+            game.onGameEnd -= OnGameEnd;
 
             initialized = false;
         }
 
-        public void StartGame()
+        private async Task ConnectRoomServerAsync()
         {
+            networkManager.networkAddress = Data.Room.room.ip;
+            networkManager.port = Data.Room.room.port;
+
+            //networkManager.onStartClient += () =>
+            //{
+            //};
+            //networkManager.onStopClient += () =>
+            //{
+            //    SceneManager.LoadScene("Lobby");
+            //};
+
             networkManager.StartClient();
+
+            await UniTask.WaitUntil(() => NetworkClient.ready);
         }
 
-        public void StopGame()
+        private async Task JoinRoomServerAsync()
+        {
+            roomNetwork.SendToServer(new GameInfoRequest());
+
+            await UniTask.WaitUntil(() => Blackboard.Contain<GameInfoResponse>());
+        }
+
+        private async Task DisconnectRoomServerAsync()
         {
             networkManager.StopClient();
+
+            await UniTask.WaitUntil(() => NetworkClient.ready == false);
+        }
+
+        public async Task StartGameAsync()
+        {
+            var gameInfoResponse = Blackboard.Read<GameInfoResponse>(erase: true);
+
+            game.Run(gameInfoResponse.GameInfo.Tick, gameInfoResponse.GameInfo.Interval, gameInfoResponse.GameInfo.ElapsedTime);
+
+            Debug.Log($"My EntityId: {gameInfoResponse.EntityId}");
+
+            //Data.User.player.entityId = gameInfoResponse.EntityId.ToString();
         }
 
         private void OnGameInfoResponse(GameInfoResponse gameInfoResponse)
         {
-            game.Run(gameInfoResponse.GameInfo.Tick, gameInfoResponse.GameInfo.Interval, gameInfoResponse.GameInfo.ElapsedTime);
+            Blackboard.Write(gameInfoResponse);
+        }
+
+        private void OnGameEnd()
+        {
+            SceneManager.LoadScene("Lobby");
         }
     }
 }

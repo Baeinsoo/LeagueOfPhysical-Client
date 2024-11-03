5a583266d8f3590c4dd4e888914d66c72d70e9b6
re5nardo <re5nardo@naver.com>
2024-11-03 15:42:36 +0900

    RoomContainer Player 추가

--- Assets/Scripts/Data/RoomContainer.cs
+++ Assets/Scripts/Data/RoomContainer.cs
@@ -8,13 +8,18 @@ namespace LOP
     {
         public RoomDto room;
         public MatchDto match;
+        public Player player;
 
-        public RoomContainer() { }
+        public RoomContainer()
+        {
+            player = new Player();
+        }
 
         public void Clear()
         {
             room = null;
             match = null;
+            player.Clear();
         }
     }
 }
---
+++ Assets/Scripts/Model/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    public class Player
+    {
+        public string entityId;
+
+        public void Clear()
+        {
+            entityId = null;
+        }
+    }
+}

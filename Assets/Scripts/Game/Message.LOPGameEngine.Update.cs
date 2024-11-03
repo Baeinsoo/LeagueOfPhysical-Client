ec827ed3642f03b4c547bbc2b0e3eda402e7a67e
re5nardo <re5nardo@naver.com>
2024-11-03 23:27:09 +0900

    LOPGameEngine.Update 메세지 추가

---
+++ Assets/Scripts/Game/Message.LOPGameEngine.Update.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP.Message.LOPGameEngine.Update
+{
+    public struct Begin { }
+    public struct BeforeEntityUpdate { }
+    public struct AfterEntityUpdate { }
+    public struct BeforePhysicsSimulation { }
+    public struct AfterPhysicsSimulation { }
+    public struct End { }
+}

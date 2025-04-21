e8539129a08ad3d8db0f4a7c88f61ea1fcbdf5e2
re5nardo <re5nardo@naver.com>
2025-04-21 21:15:17 +0900

    LOPGameEngine 수정

--- Assets/Scripts/Game/LOPGameEngine.cs
+++ Assets/Scripts/Game/LOPGameEngine.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using GameFramework;
 
@@ -8,13 +7,13 @@ namespace LOP
 {
     public class LOPGameEngine : GameEngineBase
     {
-        public static readonly IMessageBrokerExtended messageBroker = new MessageBrokerExtended();
+        public static readonly IMessageBrokerExtended updateEvents = new MessageBrokerExtended();
 
         public override void UpdateEngine()
         {
             BeginUpdate();
 
-            ProcessServerMessage();
+            ProcessNetworkMessage();
 
             ProcessInput();
 
@@ -35,63 +34,55 @@ namespace LOP
 
         private void BeginUpdate()
         {
-            messageBroker.Publish(new Message.LOPGameEngine.Update.Begin());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.Begin());
         }
 
-        private void ProcessServerMessage()
+        private void ProcessNetworkMessage()
         {
 
         }
 
         private void ProcessInput()
         {
-            foreach (var entity in entityManager.GetEntities())
-            {
-
-            }
         }
 
         private void InterpolateEntity()
         {
-
         }
 
         private void UpdateEntity()
         {
-            messageBroker.Publish(new Message.LOPGameEngine.Update.BeforeEntityUpdate());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.BeforeEntityUpdate());
 
             entityManager.UpdateEntities();
 
-            messageBroker.Publish(new Message.LOPGameEngine.Update.AfterEntityUpdate());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.AfterEntityUpdate());
         }
 
         private void UpdateAI()
         {
-
         }
 
         private void SimulatePhysics()
         {
-            messageBroker.Publish(new Message.LOPGameEngine.Update.BeforePhysicsSimulation());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.BeforePhysicsSimulation());
 
             Physics.Simulate((float)tickUpdater.interval);
 
-            messageBroker.Publish(new Message.LOPGameEngine.Update.AfterPhysicsSimulation());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.AfterPhysicsSimulation());
         }
 
         private void UpdateVisualEffect()
         {
-
         }
 
         private void ProcessEvent()
         {
-
         }
 
         private void EndUpdate()
         {
-            messageBroker.Publish(new Message.LOPGameEngine.Update.End());
+            updateEvents.Publish(new Event.LOPGameEngine.Update.End());
         }
     }
 }

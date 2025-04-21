462b5a468377550e8844f4b5d832da5a748db92a
re5nardo <re5nardo@naver.com>
2025-04-21 22:29:06 +0900

    IRoomNetwork 수정

--- Assets/Scripts/Room/IRoomNetwork.cs
+++ Assets/Scripts/Room/IRoomNetwork.cs
@@ -10,8 +10,8 @@ namespace LOP
         float latency { get; }
 
         event Action<IMessage> onMessage;
-        void SendToServer(IMessage message, bool reliable = true, bool instant = false) { }
-        void RegisterHandler<T>(Action<T> handler) where T : IMessage { }
-        void UnregisterHandler<T>(Action<T> handler) where T : IMessage { }
+        void SendToServer(IMessage message, bool reliable = true, bool instant = false);
+        void RegisterHandler<T>(Action<T> handler) where T : IMessage;
+        void UnregisterHandler<T>(Action<T> handler) where T : IMessage;
     }
 }

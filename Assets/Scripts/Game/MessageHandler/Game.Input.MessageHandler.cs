d933f57d4d8f776725d9c0b1183aa63f2404eba7
re5nardo <re5nardo@naver.com>
2025-05-11 18:52:06 +0900

    인풋 처리 로직 추가

--- Assets/Scripts/Game/MessageHandler/Game.Input.MessageHandler.cs
+++ Assets/Scripts/Game/MessageHandler/Game.Input.MessageHandler.cs
@@ -12,16 +12,60 @@ namespace LOP
         [Inject]
         private IMessageDispatcher messageDispatcher;
 
+        [Inject]
+        private IPlayerContext playerContext;
+
+        [Inject]
+        private IGameDataContext gameDataContext;
+
         public void Register()
         {
-            //messageDispatcher.RegisterHandler<EntityStatesToC>(OnEntityStatesToC, LOPRoomMessageInterceptor.Default);
-            //messageDispatcher.RegisterHandler<InputSequnceToC>(OnInputSequnceToC, LOPRoomMessageInterceptor.Default);
+            messageDispatcher.RegisterHandler<EntitySnapsToC>(OnEntitySnapsToC, LOPRoomMessageInterceptor.Default);
+            messageDispatcher.RegisterHandler<InputSequnceToC>(OnInputSequnceToC, LOPRoomMessageInterceptor.Default);
         }
 
         public void Unregister()
         {
-            //messageDispatcher.UnregisterHandler<EntityStatesToC>(OnEntityStatesToC);
-            //messageDispatcher.UnregisterHandler<InputSequnceToC>(OnInputSequnceToC);
+            messageDispatcher.UnregisterHandler<EntitySnapsToC>(OnEntitySnapsToC);
+            messageDispatcher.UnregisterHandler<InputSequnceToC>(OnInputSequnceToC);
+        }
+
+        private void OnEntitySnapsToC(EntitySnapsToC entitySnapsToC)
+        {
+            foreach (var serverEntitySnap in entitySnapsToC.EntitySnaps.OrEmpty())
+            {
+                if (gameEngine.entityManager.TryGetEntity<LOPEntity>(serverEntitySnap.EntityId, out var entity) == false)
+                {
+                    Debug.LogWarning($"Entity {serverEntitySnap.EntityId} not found");
+                    continue;
+                }
+
+                EntitySnap entitySnap = MapperConfig.mapper.Map<EntitySnap>(serverEntitySnap);
+                entitySnap.tick = entitySnapsToC.Tick;
+                entitySnap.timestamp = entitySnapsToC.Tick * gameDataContext.gameInfo.Interval;
+
+                if (playerContext.entity.entityId == entity.entityId)
+                {
+                    entity.GetComponent<SnapReconciler>().AddServerEntitySnap(entitySnap);
+                }
+                else
+                {
+                    entity.GetComponent<SnapInterpolator>().AddServerEntitySnap(entitySnap);
+                }
+            }
+        }
+
+        private void OnInputSequnceToC(InputSequnceToC inputSequnceToC)
+        {
+            InputSequnce inputSequnceData = new InputSequnce
+            {
+                Tick = inputSequnceToC.InputSequnce.Tick,
+                Sequence = inputSequnceToC.InputSequnce.Sequence,
+            };
+
+            LOPEntity lopEntity = gameEngine.entityManager.GetEntity<LOPEntity>(inputSequnceToC.EntityId);
+
+            lopEntity.GetComponent<SnapReconciler>().AddServerInputSequnce(inputSequnceData);
         }
     }
 }

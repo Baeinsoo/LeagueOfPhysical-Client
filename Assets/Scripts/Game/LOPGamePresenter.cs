9fab07a4f8c97985342c872e8a29f3532e77b881
re5nardo <re5nardo@naver.com>
2025-05-11 23:08:42 +0900

    LOPGamePresenter 추가

---
+++ Assets/Scripts/Game/LOPGamePresenter.cs
@@ -0,0 +1,61 @@
+using Cysharp.Threading.Tasks;
+using GameFramework;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+
+namespace LOP
+{
+    [DIMonoBehaviour]
+    public class LOPGamePresenter : MonoGamePresenter<LOPGame>
+    {
+        [Inject]
+        private CameraController cameraController;
+
+        [Inject]
+        private IGameDataContext gameDataContext;
+
+        [Inject]
+        private IMessageDispatcher messageDispatcher;
+
+        [Inject]
+        private IPlayerContext playerContext;
+
+        private void Awake()
+        {
+            game = GetComponent<LOPGame>();
+            game.onGameStateChanged += OnGameStateChanged;
+
+            messageDispatcher.RegisterHandler<GameInfoToC>(OnGameInfoToC, LOPRoomMessageInterceptor.Default);
+        }
+
+        private void OnDestroy()
+        {
+            game.onGameStateChanged -= OnGameStateChanged;
+            game = null;
+
+            messageDispatcher.UnregisterHandler<GameInfoToC>(OnGameInfoToC);
+        }
+
+        private void OnGameStateChanged(IGameState gameState)
+        {
+            switch (gameState)
+            {
+                case Initialized:
+                    break;
+
+                case Playing:
+                    break;
+            }
+        }
+
+        private async void OnGameInfoToC(GameInfoToC gameInfoToC)
+        {
+            await UniTask.WaitUntil(() => playerContext.entity != null && playerContext.entity.visualGameObject != null);
+
+            cameraController.SetTarget(playerContext.entity.visualGameObject.transform);
+        }
+    }
+}

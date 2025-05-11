200c13782d5e66d46e32c8aba4433ca3bf9c5501
re5nardo <re5nardo@naver.com>
2025-05-11 18:35:08 +0900

    CameraController 수정

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,32 +7,23 @@ namespace LOP
 {
     public class CameraController : MonoBehaviour
     {
-        [Header("[Camera]")]
-        [SerializeField] private Camera targetCamera;
-
-        [Header("[Settgins]")]
-        [SerializeField] private float smoothSpeed = 0.125f;
-        [SerializeField] private Vector3 offset;
-
-        public Transform target { get; set; }
-        public bool followTarget { get; set; }
-
-        private Transform myTransform;
+        [SerializeField] private CinemachineBrain cinemachineBrain;
+        [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
         private void Awake()
         {
-            myTransform = transform;
+            cinemachineBrain.m_UpdateMethod = CinemachineBrain.UpdateMethod.ManualUpdate;
+        }
+
+        public void SetTarget(Transform target)
+        {
+            cinemachineVirtualCamera.Follow = target;
+            cinemachineVirtualCamera.LookAt = target;
         }
 
         private void LateUpdate()
         {
-            if (followTarget && target != null)
-            {
-                var desiredPosition = target.position + offset;
-                var smoothedPosition = Vector3.Lerp(myTransform.position, desiredPosition, smoothSpeed);
-
-                myTransform.position = smoothedPosition;
-            }
+            cinemachineBrain.ManualUpdate();
         }
     }
 }

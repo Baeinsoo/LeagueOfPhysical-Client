e528e7b4516efd068e824f40ac1b9bd6241f59a5
re5nardo <re5nardo@naver.com>
2025-05-23 00:11:37 +0900

    점프 버튼 핸들 로직 추가

--- Assets/Scripts/Game/UI/GamePad.cs
+++ Assets/Scripts/Game/UI/GamePad.cs
@@ -1,11 +1,32 @@
+using GameFramework;
 using UnityEngine;
 using UnityEngine.UI;
+using VContainer;
 
 namespace LOP
 {
+    [DIMonoBehaviour]
     public class GamePad : MonoBehaviour
     {
         [SerializeField] private JoyStick joyStick;
         [SerializeField] private Button jumpButton;
+
+        [Inject]
+        private PlayerInputManager playerInputManager;
+
+        private void Start()
+        {
+            jumpButton.onClick.AddListener(OnJumpButtonClick);
+        }
+
+        private void OnDestroy()
+        {
+            jumpButton.onClick.RemoveListener(OnJumpButtonClick);
+        }
+
+        private void OnJumpButtonClick()
+        {
+            playerInputManager.SetJump(true);
+        }
     }
 }

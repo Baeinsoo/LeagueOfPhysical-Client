78d8b677ff3db380387c8c3fccb92bba44a08a70
re5nardo <re5nardo@naver.com>
2025-07-22 00:17:58 +0900

    Item 데이터 적용

--- Assets/Scripts/Component/ItemComponent.cs
+++ Assets/Scripts/Component/ItemComponent.cs
@@ -5,12 +5,12 @@ namespace LOP
     public class ItemComponent : LOPComponent
     {
         public string itemCode { get; private set; }
-        //public MasterData.Character masterData { get; private set; }
+        public MasterData.Item masterData { get; private set; }
 
         public void Initialize(string itemCode)
         {
-            //this.itemCode = itemCode;
-            //this.masterData = SceneLifetimeScope.Resolve<IMasterDataManager>().GetMasterData<MasterData.Character>(characterCode);
+            this.itemCode = itemCode;
+            this.masterData = SceneLifetimeScope.Resolve<IMasterDataManager>().GetMasterData<MasterData.Item>(itemCode);
         }
     }
 }
